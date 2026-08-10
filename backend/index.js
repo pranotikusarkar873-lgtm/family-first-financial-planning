6328dae027ac97c50f36297f77bcf2fcc88faeb8
Pranoti Kusarkar <pranotikusarkar873@gmail.com>
2026-08-10 14:50:57 +0530

    Update vercel.json with entrypoint and export express app

--- backend/index.js
+++ backend/index.js
@@ -29,6 +29,10 @@ app.post('/api/consultation', (req, res) => {
   }, 1000);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
